a896490b326e5ae63c0602db503014824cfefed4
Alice <blcuydy+git@gmail.com>
2014-02-05 18:35:56 +0800

    forEach, map, every, some, first, last, rest Array helpers.

--- array.js
+++ array.js
@@ -15,17 +15,42 @@ function map(func, arr){
 
 function every(func, arr){
   for(var i = 0; i < arr.length; i++){
-    if(func(arr[i])===false) return false;
+    if(func(arr[i]) === false) return false;
    }
   return true;
 }
 
+function some(func, arr){
+  for(var i = 0; i < arr.length; i++){
+    if(func(arr[i]) === true) return true;
+  }
+  return false;
+}
 
+function first(arr){
+  return arr[0]
+}
+
+function last(arr){
+  return arr[arr.length - 1]
+}
+
+function rest(arr){
+  var result = [];
+  for(var i = 1; i < arr.length; i++){
+    result[i-1] = arr[i];
+  }
+  return result;
+}
 
 module.exports = {
   forEach: forEach,
   map: map,
-  every: every
+  every: every,
+  some: some,
+  first: first,
+  last: last,
+  rest: rest
 }
 
 
