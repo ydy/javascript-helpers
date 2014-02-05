16b11d1b68c89a1b79798f8d2c68f4c3fb981987
Alice <blcuydy+git@gmail.com>
2014-02-05 18:36:21 +0800

    Test forEach, map, every, some, first, last, rest Array helpers.

--- test.js
+++ test.js
@@ -41,3 +41,53 @@ test("every", function(t){
   t.end();
 })
 
+test("some", function(t){
+  var count1 = 0;
+  var count2 = 0;
+  var result1 = arrayhelpers.some(function(item){
+    count1++;
+    if(item%3 === 0) return true;
+    else return false;
+  }, [5, 4, 3, 1])
+  
+  var result2 = arrayhelpers.some(function(item){
+    count2++;
+    if(item%5 === 0) return  true;
+    else return false;
+  }, [1, 6, 7, 11]);
+  t.equals(count1, 3, "Correct loop times");
+  t.equals(result1, true, "Correct result");
+  t.equals(count2, 4, "Correct loop times");
+  t.equals(result2, false, "Correct result");
+  t.end();
+})
+
+test("first", function(t){
+  var result = arrayhelpers.first(["a", "b", "c"]);
+  t.equals(result, "a", "Correct");
+  t.end()
+})
+
+test("last", function(t){
+  var result = arrayhelpers.last([1, 2, 3]);
+  t.equals(result, 3, "Correct");
+  t.end();
+})
+
+test("rest", function(t){
+  var result = arrayhelpers.rest([1, 2, 3, 4]);
+  t.same(result, [2, 3, 4], "Correct");
+  t.end();
+})
+
+
+
+
+
+
+
+
+
+
+
+
